67fc5e3be67f3cc71f03eaf1a7f0efe55b4d19cd
pappusaha <146526617+pappusaha@users.noreply.github.com>
2025-01-06 19:55:04 +0600

    completed first post on severSide

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express =require('express')
-const app=express()
 const cors=require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+const app=express()
 const port=process.env.PORT || 5000
-
 
 
 
@@ -10,6 +11,53 @@ const port=process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
+
+
+// from here mongodb is started 
+
+
+const uri=`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8bwej.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+console.log(uri)
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const artCollection = client.db("canvasDB").collection('artCanvas')
+ 
+    //  to read this data 
+
+    app.get('/craftItems', async (req,res) => {
+        const cursor = artCollection.find();
+        const result = await cursor.toArray()
+        res.send(result)
+
+    })
+    //  this is first post section This is Creation 
+
+    app.post('/craftItems', async (req,res)=> {
+        const  newCraftItems=req.body
+      const result= await artCollection.insertOne(newCraftItems)
+        res.send(result)
+    } )
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ping:1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 
 
